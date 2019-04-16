952b7b6b634267c6c73fcaf3917c73b4e0da243f
Soujanya Avadhani M D <38361115+soujanyaamd@users.noreply.github.com>
2019-04-17 01:57:35 +0530

    Add files via upload

---
+++ Outputs/avdss_6_res.py
@@ -0,0 +1,6 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 16:07:46) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: C:\Users\Soujanya\Desktop\AVD_SS\avdss_6.py ============
+Time Slot List [[0], [1, 4], [7, 2, 5], [6, 8], [9], [3, 10], [11, 14, 18], [12, 15, 19], [13, 16, 20], [17, 21]]
+>>> 
---
+++ Outputs/avdss_7_res.py
@@ -0,0 +1,56 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 16:07:46) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: C:\Users\Soujanya\Desktop\AVD_SS\avdss_7.py ============
+SCHEDULING AND BINDING SOLUTION
+ 
+TIMESLOT  1
+operation 1 ----> Add/Sub 1
+operation 4 ----> Add/Sub 2
+Time slot 1 ----> 1 nanoseconds
+ 
+TIMESLOT  2
+operation 7 ----> Add/Sub 1
+operation 2 ----> Mul/Div 1
+operation 5 ----> Mul/Div 2
+Time required for slot 2 ----> 3 nanoseconds
+ 
+TIMESLOT  3
+operation 6 ----> Sq 1
+operation 8 ----> Mul/Div 1
+Time required for slot 3 ----> 3 nanoseconds
+ 
+TIMESLOT  4
+operation 9 ----> Sq 1
+Time required for slot 4 ----> 2 nanoseconds
+ 
+TIMESLOT  5
+operation 3 ----> Sq 1
+operation 10 ----> Add/Sub 1
+Time required for slot 5 ----> 2 nanoseconds
+ 
+TIMESLOT  6
+operation 11 ----> Sqrt 1
+operation 14 ----> Add/Sub 1
+operation 18 ----> Add/Sub 2
+Time required for slot 6 ----> 4 nanoseconds
+ 
+TIMESLOT  7
+operation 12 ----> Mul/Div 1
+operation 15 ----> Sqrt 1
+operation 19 ----> Sqrt 2
+Time required for slot 7 ----> 4 nanoseconds
+ 
+TIMESLOT  8
+operation 13 ----> taninv 1
+operation 16 ----> Mul/Div 1
+operation 20 ----> Mul/Div 2
+Time required for slot 8 ----> 4 nanoseconds
+ 
+TIMESLOT  9
+operation 17 ----> taninv 1
+operation 21 ----> taninv 2
+Time required for slot 9 ----> 4 nanoseconds
+ 
+Total Time Required to complete the sequence of operations = 27
+>>> 
